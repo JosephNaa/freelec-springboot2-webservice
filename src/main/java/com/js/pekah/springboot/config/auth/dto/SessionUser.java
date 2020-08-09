969ab4b716230087594955db39ed7294ee534264
Joseph <js.pekah@gmail.com>
2020-08-09 18:49:32 +0900

    200809

---
+++ src/main/java/com/js/pekah/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.js.pekah.springboot.config.auth.dto;
+
+import com.js.pekah.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/com/js/pekah/springboot/service/posts/PostsService.java
+++ src/main/java/com/js/pekah/springboot/service/posts/PostsService.java
@@ -5,10 +5,13 @@ import com.js.pekah.springboot.domain.posts.PostsRepository;
 import com.js.pekah.springboot.web.dto.PostsSaveRequestDto;
 import com.js.pekah.springboot.web.dto.PostsUpdateRequestDto;
 import com.js.pekah.springboot.web.dto.PostsResponseDto;
+import com.js.pekah.springboot.web.dto.PostsListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,20 @@ public class PostsService{
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
